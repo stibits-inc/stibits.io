bc6418180a743a4107ba8b2981e6b569fd6aaa9b
Madbahaa <madbahaa@gmail.com>
2019-08-06 12:42:34 +0100

    adding contact us page

--- js/main.js
+++ js/main.js
@@ -11,7 +11,6 @@
   $(".fullscreen").css("height", window_height)
   $(".fitscreen").css("height", fitscreen);
 
-
   $(window).on('scroll', function () {
     if ($(this).scrollTop() > 600) {
       $('.scroll-top').fadeIn(600);
@@ -342,31 +341,31 @@ $('#screenshot-carusel').owlCarousel({
   // Contact Form  
   // ------------------------------------------------------------------------------ //
 
-  var submitContact = $('#submit-message'),
-    message = $('#msg');
+  // var submitContact = $('#submit-message'),
+  //   message = $('#msg');
 
-  submitContact.on('click', function (e) {
-    e.preventDefault();
+  // submitContact.on('click', function (e) {
+  //   e.preventDefault();
 
-    var $this = $(this);
+  //   var $this = $(this);
 
-    $.ajax({
-      type: "POST",
-      url: 'contact.php',
-      dataType: 'json',
-      cache: false,
-      data: $('#contact-form').serialize(),
-      success: function (data) {
+  //   $.ajax({
+  //     type: "POST",
+  //     url: 'contact.php',
+  //     dataType: 'json',
+  //     cache: false,
+  //     data: $('#contact-form').serialize(),
+  //     success: function (data) {
 
-        if (data.info !== 'error') {
-          $this.parents('form').find('input[type=text],input[type=email],textarea,select').filter(':visible').val('');
-          message.hide().removeClass('success').removeClass('error').addClass('success').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
-        } else {
-          message.hide().removeClass('success').removeClass('error').addClass('error').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
-        }
-      }
-    });
-  });
+  //       if (data.info !== 'error') {
+  //         $this.parents('form').find('input[type=text],input[type=email],textarea,select').filter(':visible').val('');
+  //         message.hide().removeClass('success').removeClass('error').addClass('success').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
+  //       } else {
+  //         message.hide().removeClass('success').removeClass('error').addClass('error').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
+  //       }
+  //     }
+  //   });
+  // });
 
 })(jQuery);
 
